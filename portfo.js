3397541e4a431711fe80dc92daac3946f958ad49
Daniel Nguyen <danngu@uw.edu>
2018-03-12 18:58:11 -0700

    Update website

--- portfo.js
+++ portfo.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
+        this.period = parseInt(period, 10) || 1000;
         this.txt = '';
         this.tick();
         this.isDeleting = false;
@@ -32,9 +32,9 @@ $(document).ready(function () {
         this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
         var that = this;
-        var delta = 300 - Math.random() * 100;
+        var delta = 100;
 
-        if (this.isDeleting) { delta /= 2; }
+        if (this.isDeleting) { delta /= 1; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
             delta = this.period;
@@ -42,7 +42,7 @@ $(document).ready(function () {
         } else if (this.isDeleting && this.txt === '') {
             this.isDeleting = false;
             this.loopNum++;
-            delta = 500;
+            delta = 100;
         }
 
         setTimeout(function () {
